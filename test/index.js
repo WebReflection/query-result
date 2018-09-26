d9c366ff903ed16c342623024e7c87aa6d42ca4d
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-09-26 10:38:50 +0100

    finalized refactoring

---
+++ test/index.js
@@ -0,0 +1,31 @@
+const tressa = require('tressa');
+const {CustomEvent, Document} = require('basicHTML');
+
+global.CustomEvent = CustomEvent;
+global.document = new Document;
+
+global.postMessage = function () {};
+global.addEventListener = global.postMessage;
+global.removeEventListener = global.postMessage;
+
+const $ = require('../cjs/index.js').default;
+
+document.body.innerHTML = '<p>1</p><p>2</p>';
+document.readyState = 'loading';
+
+tressa.title('query-result');
+
+$(event => {
+  tressa.assert(!!event, 'ready triggered');
+  document.readyState = 'whatever';
+  document.documentElement.doScroll = false;
+  $(() => {
+    const children = $('p');
+    tressa.assert(children.length === 2, 'qSA works');
+    tressa.assert($(children).length === 2, 'qr works');
+    tressa.assert($('p:first,a:first').length === 1, 'qS works');
+    tressa.assert(children instanceof $, 'instanceof works');
+  });
+});
+
+$(document).dispatch('DOMContentLoaded');
